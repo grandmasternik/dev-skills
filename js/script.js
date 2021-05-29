6cfa98b975c6c697ca0f56281ef319c011ff1843
grandmasternik <nikkimallory13@gmail.com>
2021-05-29 12:50:21 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// $addSkillButton = $('#addSkillButton');
+// // addSkillButton.on('click', addNewSkill);
+
+
+$('#addButton').click(function (event) {
+        let element = $('input').val();
+        let newRow = `<tr>
+            <td ><button class="remove">X</button></td>
+            <td >${element}</td>
+          </tr>`;
+        $('#skills tbody').append(newRow);
+    });
+
+$(document).ready(function () {
+
+    let removedSkills = [];
+
+    $('#skills tbody').on('click', 'button', function () {
+        let row = $(this).closest('tr');
+        removedSkills.push(row[0].outerHTML);
+        row.fadeOut(500, function () {
+            row.remove();
+        });
+    });
+}
